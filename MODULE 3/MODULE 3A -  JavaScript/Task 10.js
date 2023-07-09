fc530cd747d82a8e142bbd24918476fb02524bc9
Sarahedeh <Sarahedeh101@gmail.com>
2023-07-09 20:18:25 +0100

    Calculate Area of Rectangle

--- MODULE 3/MODULE 3A -  JavaScript/Task 10.js
+++ MODULE 3/MODULE 3A -  JavaScript/Task 10.js
@@ -1,6 +1,8 @@
+// A programming code for calculating Area of Rectangle
 function calculateAreaOfRectangle(lenght, width){
     const product = lenght * width
     return product;
     }
     const moreOp = (calculateAreaOfRectangle(3,4))+10;
     console.log(moreOp);
+
