7ecbee3acafc3ae0a3e423bc1611fadb0e999c5b
Sarahedeh <Sarahedeh101@gmail.com>
2023-07-09 19:29:01 +0100

    Introduction_To_Object

---
+++ MODULE 3/MODULE 3A -  JavaScript/Task13.js
@@ -0,0 +1,9 @@
+const book = {
+    ttle:('Sweet Potato'),
+    description:('How to fry Potato'),
+    numberOfPages: 43,
+    author: ('Sarah'),
+    reading:('true'),
+}
+
+console.log(book.numberOfPages)
