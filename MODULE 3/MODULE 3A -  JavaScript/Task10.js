58f77a9526ed3d040e485800c891f28cf2e15eab
Sarahedeh <Sarahedeh101@gmail.com>
2023-07-09 17:59:03 +0100

    calculateAreaOfRectangle

---
+++ MODULE 3/MODULE 3A -  JavaScript/Task10.js
@@ -0,0 +1,10 @@
+//function calculateAreaOfRectangle(lenght, width){
+//const product = lenght * width
+//return product;
+//}
+//const moreOp = (calculateAreaOfRectangle(3,4))+10;
+//console.log(moreOp);
+
+const number =['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+number.slice(2,6)
+console.log(number.slice(2,6))
