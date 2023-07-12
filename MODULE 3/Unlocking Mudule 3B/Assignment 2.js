368ace343c24439b8356996e3ac3dd8e10f7b6aa
Sarahedeh <Sarahedeh101@gmail.com>
2023-07-12 15:48:26 +0100

    Code for creating a lenght converter function

--- MODULE 3/Unlocking Mudule 3B/Assignment 2.js
+++ MODULE 3/Unlocking Mudule 3B/Assignment 2.js
@@ -1,8 +1,9 @@
 //Code for creating a lenght converter function
 
 function converter(millimeter){
-    const centimeter = millimeter * 50000;
-    return centimeter;
+    const centimeter = millimeter * 50000;  
+return centimeter;
+
 }
 const centimeterValue = converter(50000);
 console.log(centimeterValue)
