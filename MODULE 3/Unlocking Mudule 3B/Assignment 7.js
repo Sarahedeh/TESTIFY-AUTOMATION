0fad511e19521ff3b3704f1e974d1cfb5cd0a8a5
Sarahedeh <Sarahedeh101@gmail.com>
2023-07-12 15:32:09 +0100

    code for sorting an array of number in descending order

---
+++ MODULE 3/Unlocking Mudule 3B/Assignment 7.js
@@ -0,0 +1,6 @@
+//code for sorting an array of number in descending order
+
+
+const numbers = [40, 100, 1, 5, 25, 10, 3, 7, 9, 6, 2];
+numbers.sort(function(a, b){return b-a});
+console.log(numbers)
