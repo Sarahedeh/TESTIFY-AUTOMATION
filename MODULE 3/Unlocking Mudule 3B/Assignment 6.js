4a64e46815846bbd5805b1bf9b5d7279c41d50ef
Sarahedeh <Sarahedeh101@gmail.com>
2023-07-12 15:06:20 +0100

    code for sorting an array in alphabetical order

---
+++ MODULE 3/Unlocking Mudule 3B/Assignment 6.js
@@ -0,0 +1,4 @@
+//code for sorting an array of strings in alphabetical order
+ const strings = ['Sarah', 'Edeh', 'Ngozi', 'Chinyere', 'Jide', 'Peace', 'Robert', 'Light']
+ strings.sort();
+ console.log(strings)
