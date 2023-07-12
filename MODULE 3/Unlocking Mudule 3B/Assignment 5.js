2d10284f687672116752ff00b86af9d63830669f
Sarahedeh <Sarahedeh101@gmail.com>
2023-07-12 13:26:01 +0100

    Code for creating a function that reverse an array

--- MODULE 3/Unlocking Mudule 3B/Assignment 5.js
+++ MODULE 3/Unlocking Mudule 3B/Assignment 5.js
@@ -1,1 +1,7 @@
 //Code for creating a function that reverse an array
+
+const myArray = [20, 40, 80, 100];
+const revMyArr = [].concat(myArray).reverse();
+console.log(revMyArr);
+// [100, 80, 40, 20]
+
